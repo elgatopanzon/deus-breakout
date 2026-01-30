e3d608abf2fcc2bdd2a21214ba2c0dd597bfe252
ElGatoPanzon <gato@elgatopanzon.io>
2026-01-29 22:03:53 -0600

    Add main scene with pipeline bootstrap and set as run target

---
+++ scenes/Main.gd
@@ -0,0 +1,17 @@
+######################################################################
+# @author      : ElGatoPanzon
+# @class       : Main
+# @created     : Thursday Jan 29, 2026 20:45:00 CST
+# @copyright   : Copyright (c) ElGatoPanzon 2026
+#
+# @description : game bootstrap — registers pipelines with scheduler
+######################################################################
+
+extends Node2D
+
+func _ready():
+	for pipeline in [PaddleInputPipeline, MovementPipeline, PositionClampPipeline]:
+		Deus.register_pipeline(pipeline)
+		Deus.pipeline_scheduler.register_task(
+			PipelineSchedulerDefaults.OnUpdate, pipeline
+		)
